03f0bdc2b6de0fd080fdae670e0c5b7ed643e136
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-12 15:42:16 +0700

    add color and update funtion call

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,9 +1,11 @@
+import { type ColorSchemeName } from 'react-native'
 const tintColorLight = '#2f95dc'
 const tintColorDark = '#fff'
-
-export default {
+const colors = {
   dark: {
     background: '#000',
+    cornflowerBlue: '#5B9EE1',
+    midnightBlue: '#1A2530',
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
     text: '#fff',
@@ -11,9 +13,16 @@ export default {
   },
   light: {
     background: '#fff',
+    lightGray: '#F2F2F2',
+    slateGray: '#707B81',
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
     text: '#000',
     tint: tintColorLight
   }
 }
+
+const getColors = (colorScheme: ColorSchemeName): any => {
+  return colorScheme === 'dark' ? colors.dark : colors.light
+}
+export default getColors

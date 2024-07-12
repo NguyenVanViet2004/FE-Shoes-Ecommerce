45380362f753bfab57a4557a0b91b4c005b87281
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-12 15:30:55 +0700

    check the first time to use the app

--- app/index.tsx
+++ app/index.tsx
@@ -1,24 +1,72 @@
-import { StyleSheet } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { Text } from 'tamagui'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
+import { View } from 'tamagui'
+
+// import OnboardingTemplate from '~/components/templates/OnboardingTemplate'
+import SplashTemplate from '~/components/templates/SplashTemplate'
+import useStorage from '~/hooks/useStorage'
 
 const index = (): JSX.Element => {
+  const { getItem, setItem } = useStorage()
+  const [firstTime, setFirstTime] = useState(true)
+  const [loading, setLoading] = useState(true)
+
+  const FIRST_TIME_USE_APP = 'firstTimeUseApp'
+
+  useEffect(() => {
+    const checkFirstTime = async (): Promise<void> => {
+      try {
+        const user = await getItem(FIRST_TIME_USE_APP)
+        if (user === undefined) {
+          setFirstTime(false)
+        }
+      } catch (error) {
+        console.log('Error check first time:', error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    checkFirstTime().catch(e => {
+      console.log('Error checkFirstTime:', e)
+    })
+  }, [getItem])
+
+  useLayoutEffect(() => {
+    if (!loading && firstTime) {
+      console.log('move to Login')
+    }
+  }, [loading])
+
+  useLayoutEffect(() => {
+    if (!loading && !firstTime) {
+      console.log('move to Onboarding')
+    }
+  }, [loading])
+
+  useEffect(() => {
+    const setUseApp = async (): Promise<void> => {
+      try {
+        await setItem(FIRST_TIME_USE_APP, 'true')
+      } catch (error) {
+        console.log('Error setting used app status:', error)
+      }
+    }
+
+    setUseApp().catch((error) => {
+      console.log('Error setUsedApp:', error)
+    })
+  }, [firstTime, setItem])
+
+  if (loading) {
+    return <SplashTemplate />
+  }
+
   return (
-    <SafeAreaView style={styles.container}>
-      <Text
-        textAlign="center"
-        color={'red'} fontSize={20}
-        fontWeight={'bold'}>FE Shoe Ecommerce</Text>
-    </SafeAreaView>
+    <View flex={1}>
+      <SplashTemplate/>
+      {/* <OnboardingTemplate/> */}
+    </View>
   )
 }
 
 export default index
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center'
-  }
-})

14c1a03f576df832af5c4ac0b92a6af1baf7abde
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-13 13:01:07 +0700

    add color

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -5,19 +5,25 @@ const colors = {
   dark: {
     background: '#000',
     cornflowerBlue: '#5B9EE1',
+    lightGray: '#F2F2F2',
     midnightBlue: '#1A2530',
+    slateGray: '#707B81',
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorDark,
     text: '#fff',
+    textButton: '#FFFFFF',
     tint: tintColorDark
   },
   light: {
     background: '#fff',
+    cornflowerBlue: '#5B9EE1',
     lightGray: '#F2F2F2',
+    midnightBlue: '#1A2530',
     slateGray: '#707B81',
     tabIconDefault: '#ccc',
     tabIconSelected: tintColorLight,
     text: '#000',
+    textButton: '#FFFFFF',
     tint: tintColorLight
   }
 }

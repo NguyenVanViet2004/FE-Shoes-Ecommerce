b0529df1a2e020d46696c52c558ae7b1f2855690
Truong <truongtdph40209@fpt.edu.vn>
2024-07-17 14:47:40 +0700

    FIx component and change touchableopacity to button

--- components/molecules/common/Footer.tsx
+++ components/molecules/common/Footer.tsx
@@ -1,41 +1,42 @@
 import React from 'react'
-import { TouchableOpacity, useColorScheme } from 'react-native'
-import { Text, XStack, YStack } from 'tamagui'
+import { useColorScheme } from 'react-native'
+import { Button, Text, XStack, YStack } from 'tamagui'
+
 import getColors from '~/constants/Colors'
 
-interface props {
+interface Props {
   title: string
   subtitle: string
 }
-const FooterCompoment: React.FC<props> = ({ title, subtitle }) => {
+
+const FooterComponent: React.FC<Props> = ({ title, subtitle }) => {
   const colors = getColors(useColorScheme())
+
   return (
-    <YStack
-      marginTop={150}
-    >
-      <XStack
-        justifyContent="center"
-        gap={8}
-      >
+    <YStack justifyContent="flex-end" flex={1}>
+      <XStack justifyContent="center" gap={8} bottom={50}>
+
         <Text
           fontSize={12}
           color={colors.slateGray}
           textAlign="center"
-          fontWeight="400"
-        >
+          fontWeight="400">
           {title}
         </Text>
-        <TouchableOpacity>
+
+        <Button unstyled>
           <Text
             fontSize={12}
             fontWeight="bold"
             color={colors.midnightBlue}>
             {subtitle}
           </Text>
-        </TouchableOpacity>
+        </Button>
+
       </XStack>
     </YStack>
+
   )
 }
 
-export default FooterCompoment
+export default FooterComponent

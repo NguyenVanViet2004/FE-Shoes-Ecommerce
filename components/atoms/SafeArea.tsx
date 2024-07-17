a8988c023b3d91db7c4449b77c95b412360a9e11
Truong <truongtdph40209@fpt.edu.vn>
2024-07-17 14:37:06 +0700

    Change SafeAreaView library

--- components/atoms/SafeArea.tsx
+++ components/atoms/SafeArea.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useColorScheme, type ViewStyle } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import { SafeAreaView, useColorScheme, type ViewStyle } from 'react-native'
+
 import getColors from '~/constants/Colors'
 
 interface Props {

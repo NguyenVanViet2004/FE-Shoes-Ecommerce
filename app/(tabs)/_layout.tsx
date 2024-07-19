ebdbdb0b3c55f7c70102a5f2ceeac997963878da
Nguyen Van Viet <nviet03052004@gmail.com>
2024-07-19 12:53:14 +0700

    Replace tabs folder with BottomBar

--- app/(tabs)/_layout.tsx
+++
@@ -1,8 +0,0 @@
-import BottomTabBar from '~/components/templates/BottomTab'
-
-export default function TabLayout(): React.ReactElement {
-
-  return (
-    <BottomTabBar />
-  )
-}
--- app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, YStack } from 'tamagui'
-
-export default function TabOneScreen(): JSX.Element {
-  return (
-    <YStack f={1} ai="center" gap="$8" px="$10" pt="$5">
-      <Text>Tab one</Text>
-    </YStack>
-  )
-}

a8746a03e7982be68153d4425a967b4bea2ab094
Nguyen Van Viet <nviet03052004@gmail.com>
2024-07-19 12:47:46 +0700

    Refactor the constants folder:
    - Added a TabItems component
    
    - Fix lint for Colors.ts

---
+++ constants/TabItems.ts
@@ -0,0 +1,54 @@
+import { Icons } from '~/components/atoms/Icons'
+import AccountScreen from '~/components/templates/AccountTemplate'
+import Favorite from '~/components/templates/FavoriteScreen'
+import HomeScreen from '~/components/templates/HomeTemplate'
+import NotificationScreen from '~/components/templates/NotificationTemplate'
+import OderScreen from '~/components/templates/OderTemplate'
+
+export interface TabItem {
+  route: string
+  label: string
+  type: any
+  icon: string
+  component: React.ComponentType<any>
+}
+
+const TabItems: TabItem[] = [
+  {
+    component: HomeScreen,
+    icon: 'home',
+    label: 'Home',
+    route: 'Home',
+    type: Icons.AntDesign
+  },
+  {
+    component: Favorite,
+    icon: 'heart',
+    label: 'Favorite',
+    route: 'Favorite',
+    type: Icons.Feather
+  },
+  {
+    component: OderScreen,
+    icon: 'shopping-cart',
+    label: 'Oder',
+    route: 'Oder',
+    type: Icons.Feather
+  },
+  {
+    component: NotificationScreen,
+    icon: 'notifications-none',
+    label: 'Notify',
+    route: 'Notifications',
+    type: Icons.MaterialIcons
+  },
+  {
+    component: AccountScreen,
+    icon: 'user-circle-o',
+    label: 'Account',
+    route: 'Account',
+    type: Icons.FontAwesome
+  }
+]
+
+export default TabItems

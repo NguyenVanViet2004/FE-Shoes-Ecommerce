397fdfb3ac42e05d70c49cbf5ee9db3cc33aad30
Truong <truongtdph40209@fpt.edu.vn>
2024-07-19 09:31:52 +0700

    Add SignUpTemplate component

---
+++ components/templates/SignUpTemplate.tsx
@@ -0,0 +1,64 @@
+import { AntDesign } from '@expo/vector-icons'
+import { useRouter } from 'expo-router'
+import React from 'react'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { Button } from 'tamagui'
+
+import SafeArea from '~/components/atoms/SafeArea'
+import FooterComponent from '~/components/molecules/common/Footer'
+import Header from '~/components/molecules/common/Header'
+import getColors from '~/constants/Colors'
+import useTranslation from '~/hooks/useTranslation'
+
+import SignUpForm from '../molecules/SignUpForm'
+
+const SignUpTemplate: React.FC = (): JSX.Element => {
+  const { t } = useTranslation()
+  const colors = getColors(useColorScheme())
+  const router = useRouter()
+
+  const handleBack = (): void => {
+    router.back()
+  }
+  const handleSignIn = (): void => {
+  }
+
+  return (
+    <SafeArea style={{
+      ...styles.container,
+      backgroundColor: colors.lightSilver
+    }}>
+      <Header
+        title={t('signUp.createAccount')}
+        subtitle={t('signUp.letsCreateAccountTogether')}
+        leftIcon={
+          <Button
+            unstyled
+            onPress={handleBack}
+            padding={10}
+            borderRadius={50}
+            backgroundColor={colors.white}
+            alignSelf="baseline">
+            <AntDesign name="left" size={18}
+              color={colors.black} />
+          </Button>
+        } />
+
+      <SignUpForm />
+
+      <FooterComponent
+        onPress={handleSignIn}
+        title={t('Already have an account?')}
+        subtitle={t('signUp.signIn')} />
+
+    </SafeArea>
+  )
+}
+export default SignUpTemplate
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 20
+  }
+})

607cdcd2828023dce6fd4a1d7bc3e485223f9e6a
tudev <tukieu954@gmail.com>
2024-07-21 20:19:05 +0700

    Create components Header Details

--- components/molecules/HeaderDetails.tsx
+++ components/molecules/HeaderDetails.tsx
@@ -1,47 +1,50 @@
-import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-import Entypo from '@expo/vector-icons/Entypo';
-import Feather from '@expo/vector-icons/Feather';
+import Entypo from '@expo/vector-icons/Entypo'
+import Feather from '@expo/vector-icons/Feather'
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, useColorScheme, View } from 'react-native'
+
+import getColors from '~/constants/Colors'
+const colors = getColors(useColorScheme())
 const HeaderDetails: React.FC = () => {
-    return (
-        <View style={styles.container}>
-            <TouchableOpacity style={styles.iconButton}>
-                <Entypo name="chevron-left" size={24} color="black" />
-            </TouchableOpacity>
-            <Text style={styles.title}>Men’s Shoes</Text>
-            <TouchableOpacity style={styles.iconButton}>
-                <Feather name="shopping-cart" size={24} color="black" />
-            </TouchableOpacity>
-        </View>
-    );
-};
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.iconButton}>
+        <Entypo name="chevron-left" size={24} color={colors.black} />
+      </TouchableOpacity>
+      <Text style={styles.title}>Men’s Shoes</Text>
+      <TouchableOpacity style={styles.iconButton}>
+        <Feather name="shopping-cart" size={24} color={colors.black} />
+      </TouchableOpacity>
+    </View>
+  )
+}
 
 const styles = StyleSheet.create({
-    container: {
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        alignItems: 'center',
-        padding: 16,
-        backgroundColor: '#f5f5f5',
-    },
-    iconButton: {
-        width: 40,
-        height: 40,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#fff',
-        borderRadius: 20,
-        shadowColor: '#000',
-        shadowOffset: { width: 0, height: 2 },
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        elevation: 3,
-    },
-    title: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        color: '#000',
-    },
-});
+  container: {
+    alignItems: 'center',
+    backgroundColor: colors.whiteSmke,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 16
+  },
+  iconButton: {
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    borderRadius: 20,
+    elevation: 3,
+    height: 40,
+    justifyContent: 'center',
+    shadowColor: colors.black,
+    shadowOffset: { height: 2, width: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    width: 40
+  },
+  title: {
+    color: colors.black,
+    fontSize: 18,
+    fontWeight: 'bold'
+  }
+})
 
-export default HeaderDetails;
+export default HeaderDetails

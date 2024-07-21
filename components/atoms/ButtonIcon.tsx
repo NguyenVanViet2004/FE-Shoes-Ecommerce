60169df1e0945749ec542dfb13f41357b2974394
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-21 14:53:03 +0700

    create component ButtonIcon in atoms

---
+++ components/atoms/ButtonIcon.tsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import { StyleSheet, useColorScheme, View } from 'react-native'
+import { Button, type ButtonProps, Image } from 'tamagui'
+
+import getColors from '~/constants/Colors'
+
+type Props = {
+  isSelected?: boolean
+  icon: number | React.ReactElement
+} & ButtonProps
+
+export const ButtonIcon = (props: Props): React.ReactElement => {
+  const colors = getColors(useColorScheme())
+
+  return (
+    <Button
+      unstyled
+      backgroundColor={colors.white}
+      borderRadius={30}
+      alignItems="center"
+      justifyContent="center"
+      onPress={props.onPress}
+      style={props.style}>
+      <View
+        style={
+          typeof props.icon === 'number'
+            ? styles.iconWrapperWithImage
+            : styles.iconWrapper
+        }>
+
+        {typeof props.icon === 'number'
+          ? (
+            <Image
+              source={props.icon}
+              style={[
+                styles.img,
+                props.isSelected === true && styles.selectedImg
+              ]}
+            />)
+          : (props.icon)
+        }
+      </View>
+    </Button>
+  )
+}
+
+const styles = StyleSheet.create({
+  iconWrapper: {
+    padding: 0
+  },
+  iconWrapperWithImage: {
+    padding: 8
+  },
+  img: {
+    height: 25,
+    resizeMode: 'contain',
+    width: 25
+  },
+  selectedImg: {
+    height: 20,
+    width: 20
+  }
+})

f8dfd38d2d5a55d696555dd450236111325f75f3
tudev <tukieu954@gmail.com>
2024-07-23 18:22:39 +0700

    Fix gallery component ,used style tamagui

--- components/atoms/Gallery.tsx
+++ components/atoms/Gallery.tsx
@@ -1,47 +1,46 @@
 import React from 'react'
-import { Image, StyleSheet, Text, View, ImageSourcePropType } from 'react-native'
+import {
+  Image,
+  type ImageSourcePropType,
+  StyleSheet,
+  useColorScheme
+} from 'react-native'
+import { Text, View } from 'tamagui'
 
+import getColors from '~/constants/Colors'
+
+const colors = getColors(useColorScheme())
 interface GalleryProps {
-    images: ImageSourcePropType[]
-    colors: {
-        primary: string
-        secondary: string
-    }
+  images: ImageSourcePropType[]
+  colors: {
+    primary: string
+    secondary: string
+  }
 }
 
 const Gallery: React.FC<GalleryProps> = ({ images, colors }) => {
-    return (
-        <View style={styles.container}>
-            <Text style={styles.galleryText}>Gallery</Text>
-            <View style={styles.galleryContainer}>
-                {images.map((img, index) => (
-                    <Image key={index} source={img} style={styles.galleryImage} />
-                ))}
-            </View>
-        </View>
-    )
+  return (
+    <View marginHorizontal={18}>
+      <Text fontSize={18} fontWeight={'bold'} marginVertical={7}>
+        Gallery
+      </Text>
+      <View flexDirection="row" gap={10} justifyContent="flex-start">
+        {images.map((img, index) => (
+          <Image key={index} source={img} style={styles.galleryImage} />
+        ))}
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    container: {
-        marginHorizontal: 18
-    },
-    galleryContainer: {
-        flexDirection: 'row',
-        gap: 10,
-        justifyContent: 'flex-start'
-    },
-    galleryImage: {
-        borderRadius: 10,
-        borderWidth: 1,
-        height: 60,
-        width: 60
-    },
-    galleryText: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginVertical: 7
-    }
+  galleryImage: {
+    backgroundColor: colors.whiteSmoke,
+    borderRadius: 10,
+    borderWidth: 1,
+    height: 60,
+    width: 60
+  }
 })
 
 export default Gallery

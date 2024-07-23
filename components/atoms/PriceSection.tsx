5ab5529d02569224be473374b4bc376f1211e0ad
tudev <tukieu954@gmail.com>
2024-07-23 18:26:21 +0700

    Fix priceSection component , used to style tamagui , fix UI same figma

--- components/atoms/PriceSection.tsx
+++ components/atoms/PriceSection.tsx
@@ -1,71 +1,64 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity, View, ViewStyle, TextStyle, useColorScheme } from 'react-native'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { Button, Text, View } from 'tamagui'
+
 import getColors from '~/constants/Colors'
 interface Colors {
-    blue: string
-    white: string
-    darkGray: string
-    black: string
+  blue: string
+  white: string
+  darkGray: string
+  black: string
 }
 
 interface PriceSectionProps {
-    colors: Colors
+  colors: Colors
 }
 const colors = getColors(useColorScheme())
 
 const PriceSection: React.FC<PriceSectionProps> = ({ colors }) => {
-    return (
-        <View style={styles.priceContainer}>
-            <View>
-                <Text style={styles.finalPrice}>Price</Text>
-                <Text style={styles.finalPriceValue}>$849.69</Text>
-            </View>
-            <View>
-                <TouchableOpacity style={styles.addToCartButton}>
-                    <Text style={styles.addToCartText}>Add To Cart</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    )
+  return (
+    <View style={styles.priceContainer}>
+      <View>
+        <Text color={colors.darkGray} fontSize={16}>
+          Price
+        </Text>
+        <Text fontSize={20} fontWeight="bold" marginVertical={7}>
+          $849.69
+        </Text>
+      </View>
+      <View>
+        <Button
+          backgroundColor={colors.blue}
+          borderRadius={24}
+          paddingHorizontal={24}
+          paddingVertical={12}
+        >
+          <Text color={colors.white} fontSize={16} fontWeight="bold">
+            Add To Cart
+          </Text>
+        </Button>
+      </View>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-    addToCartButton: {
-        backgroundColor: colors.blue,
-        borderRadius: 24,
-        paddingHorizontal: 24,
-        paddingVertical: 12
-    },
-    addToCartText: {
-        color: colors.white,
-        fontSize: 18,
-        fontWeight: 'bold'
-    } as TextStyle,
-    finalPrice: {
-        color: colors.darkGray,
-        fontSize: 16,
-    } as TextStyle,
-    finalPriceValue: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        marginVertical: 7
-    } as TextStyle,
-    priceContainer: {
-        alignItems: 'center',
-        backgroundColor: colors.white,
-        borderRadius: 12,
-        elevation: 3,
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        marginTop: 20,
-        padding: 16,
-        shadowColor: colors.black,
-        shadowOffset: { height: 2, width: 0 },
-        shadowOpacity: 0.1,
-        shadowRadius: 8,
-        marginHorizontal: 10,
-        marginVertical: 10,
-    } as ViewStyle
+  priceContainer: {
+    alignItems: 'center',
+    backgroundColor: colors.white,
+    borderRadius: 12,
+    elevation: 3,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginHorizontal: 10,
+    marginTop: 20,
+    marginVertical: 10,
+    padding: 16,
+    shadowColor: colors.black,
+    shadowOffset: { height: 2, width: 0 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8
+  }
 })
 
 export default PriceSection

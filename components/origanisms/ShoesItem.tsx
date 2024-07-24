047f54ed74389c650a34050a3cc3ce010b4f7611
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-25 02:08:43 +0700

    create component ShoesItem to dislay New Arrivals

---
+++ components/origanisms/ShoesItem.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useColorScheme } from 'react-native'
+import { Image, View, type ViewProps } from 'tamagui'
+
+import { InforShoes } from '~/components/molecules/InforShoes'
+import getColors from '~/constants/Colors'
+import useTranslation from '~/hooks/useTranslation'
+
+type Props = {
+  label: string
+  nameShoes: string
+  price: string
+} & ViewProps
+
+export const ShoesItem = (props: Props): React.ReactElement => {
+  const colors = getColors(useColorScheme())
+  const { t } = useTranslation()
+
+  return (
+    <View
+      alignItems="center"
+      borderRadius={16}
+      padding={20}
+      flexDirection="row"
+      justifyContent="space-between"
+      backgroundColor={colors.white}>
+      <InforShoes
+        gap={8}
+        flex={1}
+        label={t('home.' + props.label)}
+        nameShoes={props.nameShoes}
+        priceShoes={props.price} />
+      <Image
+        source={require('assets/images/shoes2.png')}
+        flex={1}
+        aspectRatio={1}
+        resizeMethod="auto"
+        maxHeight={90}
+        maxWidth={120} />
+    </View>
+  )
+}

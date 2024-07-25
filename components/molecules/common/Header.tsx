c563a1ff0ed1232196750275df98ac0d97ac79a8
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-26 02:01:36 +0700

    add prop label

--- components/molecules/common/Header.tsx
+++ components/molecules/common/Header.tsx
@@ -10,44 +10,54 @@ interface props {
   rightIcon?: number | React.ReactElement
   title?: string
   subtitle?: string
+  label?: string
 }
 
-const Header: React.FC<props> = ({ leftIcon, rightIcon, title, subtitle }) => {
-  const colors = getColors(useColorScheme())
-  const renderIcon = (icon: number | ReactElement): React.ReactElement => {
-    if (isNumber(icon)) {
-      return <Image source={icon} />
+const Header: React.FC<props> =
+  ({ leftIcon, rightIcon, title, subtitle, label }) => {
+    const colors = getColors(useColorScheme())
+    const renderIcon = (icon: number | ReactElement): React.ReactElement => {
+      if (isNumber(icon)) {
+        return <Image source={icon} />
+      }
+      return <Button
+        unstyled
+        padding={10}
+        borderRadius={50}
+        backgroundColor={colors.white}
+        alignSelf="baseline">{icon}</Button>
     }
-    return <Button unstyled
-      padding={10}
-      borderRadius={50}
-      backgroundColor={colors.white}
-      alignSelf="baseline">{icon}</Button>
-  }
 
-  return (
-    <YStack marginTop={20}>
-      <XStack alignItems="center" justifyContent="space-between">
-        {!isNil(leftIcon) && renderIcon(leftIcon)}
-        <YStack
-          alignItems="center"
-          gap={10}>
-          <Text
-            fontSize={28}
-            fontWeight="bold"
-            color={colors.midnightBlue}>
-            {title}
-          </Text>
-          <Text
-            fontSize={16}
-            fontWeight="500"
-            color={colors.slateGray}>
-            {subtitle}
-          </Text>
-        </YStack>
-        {!isNil(rightIcon) && renderIcon(rightIcon)}
-      </XStack>
-    </YStack>
-  )
-}
+    const renderLabel = (label: string): React.ReactElement => {
+      return (
+        <Text fontSize={16} fontWeight={500} >{label}</Text>
+      )
+    }
+
+    return (
+      <YStack marginTop={20}>
+        <XStack alignItems="center" justifyContent="space-between" >
+          {!isNil(leftIcon) && renderIcon(leftIcon)}
+
+          {!isNil(label) && (
+            <XStack flex={1} alignItems="center" justifyContent="center">
+              {renderLabel(label)}
+            </XStack>
+          )}
+
+          {!isNil(title) && !isNil(subtitle) && (
+            <YStack alignItems="center" gap={10}>
+              <Text fontSize={28} fontWeight="bold" color={colors.midnightBlue}>
+                {title}
+              </Text>
+              <Text fontSize={16} fontWeight={500} color={colors.slateGray}>
+                {subtitle}
+              </Text>
+            </YStack>
+          )}
+          {!isNil(rightIcon) && renderIcon(rightIcon)}
+        </XStack>
+      </YStack>
+    )
+  }
 export default Header

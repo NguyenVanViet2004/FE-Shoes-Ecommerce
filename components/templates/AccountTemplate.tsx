a6ef7755138f45730aecb7eda521ede9f8656bcd
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-07-26 02:17:04 +0700

    implement AccountTemplate

--- components/templates/AccountTemplate.tsx
+++ components/templates/AccountTemplate.tsx
@@ -1,22 +1,98 @@
+import { AntDesign, Ionicons, MaterialCommunityIcons } from '@expo/vector-icons'
 import React from 'react'
-import { SafeAreaView, StyleSheet } from 'react-native'
-import { Text } from 'tamagui'
+import { StyleSheet, useColorScheme } from 'react-native'
+import { ScrollView, Text, View } from 'tamagui'
+
+import Header from '~/components/molecules/common/Header'
+import getColors from '~/constants/Colors'
+
+import SafeArea from '../atoms/SafeArea'
+import { ChooseMethod } from '../molecules/ChooseMethod'
 const AccountScreen: React.FC = () => {
+  const colors = getColors(useColorScheme())
   return (
-    <SafeAreaView style={styles.container}>
-      <Text
-        textAlign="center"
-        fontSize={20}
-      >AccountScreen</Text>
-    </SafeAreaView>
+    <SafeArea style={styles.container}>
+      <ScrollView>
+        <View
+          flex={1}
+          paddingBottom={120}
+          paddingHorizontal={20}
+          backgroundColor={colors.lightSilver}>
+          <Header
+            leftIcon={
+              <AntDesign name="left" size={15} color={colors.midnightBlue} />}
+            label="Account & Settings" />
+
+          <Text
+            fontSize={18}
+            fontWeight={500}
+            marginVertical={24}>Account</Text>
+
+          <ChooseMethod
+            leftIcon={<MaterialCommunityIcons
+              name="bell-ring-outline"
+              size={24}
+              color={colors.slateGray} />}
+            rightIcon={<AntDesign
+              name="right"
+              size={15}
+              color={colors.slateGray} />}
+            nameMethod="notificationSetting" />
+
+          <ChooseMethod
+            leftIcon={<Ionicons
+              name="bag-handle-outline"
+              size={24}
+              color={colors.slateGray} />}
+            rightIcon={<AntDesign
+              name="right"
+              size={15}
+              color={colors.slateGray} />}
+            nameMethod="shippingAddress" />
+
+          <ChooseMethod
+            leftIcon={<AntDesign
+              name="wallet"
+              size={24}
+              color={colors.slateGray} />}
+            rightIcon={<AntDesign
+              name="right"
+              size={15}
+              color={colors.slateGray} />}
+            nameMethod="paymentInfo" />
+
+          <ChooseMethod
+            leftIcon={<AntDesign
+              name="delete"
+              size={24}
+              color={colors.slateGray} />}
+            rightIcon={<AntDesign
+              name="right"
+              size={15}
+              color={colors.slateGray} />}
+            nameMethod="deleteAccount" />
+
+          <Text
+            fontSize={18}
+            fontWeight={500}
+            marginVertical={24}>App Settings</Text>
+
+          <ChooseMethod nameMethod="eneble-FaceID-For-LogIn" useSwitch />
+
+          <ChooseMethod nameMethod="eneblePushNotifications" useSwitch />
+
+          <ChooseMethod nameMethod="enebleLocationServices" useSwitch />
+
+          <ChooseMethod nameMethod="darkMode" useSwitch />
+        </View>
+      </ScrollView>
+    </SafeArea>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    flex: 1,
-    justifyContent: 'center'
+    flex: 1
   }
 })
 

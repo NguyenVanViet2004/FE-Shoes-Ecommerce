f1e25bf6f0849a1686120ff3cd2c37beb2bd5209
tudev <tukieu954@gmail.com>
2024-07-26 16:36:13 +0700

    Fix style tamagui, use translation , Eliminate all the sizes are over , update don't pay if size of gallety over

--- components/atoms/SizeSelector.tsx
+++ components/atoms/SizeSelector.tsx
@@ -1,114 +1,91 @@
 import React from 'react'
-import {
-  StyleSheet,
-  TouchableOpacity,
-  useColorScheme
-} from 'react-native'
-import { Text, View } from 'tamagui'
+import { useColorScheme } from 'react-native'
+import { Button, Text, XStack, YStack } from 'tamagui'
 
 import getColors from '~/constants/Colors'
-interface Colors {
-  blue: string
-  whiteSmoke: string
-  black: string
-  darkGray: string
-}
-const colors = getColors(useColorScheme())
 
 interface SizeSelectorProps {
   sizes: string[]
   selectedSize: string
   setSelectedSize: (size: string) => void
-  colors: Colors
+  outOfStockSizes: string[]
 }
 
 const SizeSelector: React.FC<SizeSelectorProps> = ({
   sizes,
   selectedSize,
   setSelectedSize,
-  colors
+  outOfStockSizes
 }) => {
+  const colors = getColors(useColorScheme())
+
   return (
-    <View marginHorizontal={18}>
-      <View
-        alignItems="center"
-        flexDirection="row"
-        justifyContent="space-between"
-      >
+    <YStack marginHorizontal={18} marginTop={10}>
+      <XStack alignItems="center" justifyContent="space-between">
         <Text fontSize={18} fontWeight="bold">
           Size
         </Text>
-        <View style={styles.sizeOptions}>
-          <Text style={[styles.sizeOption, styles.activeSizeOption]}>EU</Text>
-          <Text style={styles.sizeOption}>US</Text>
-          <Text style={styles.sizeOption}>UK</Text>
-        </View>
-      </View>
-      <View
-        borderBottomColor={colors.blue}
+        <XStack flexDirection="row">
+          <Text marginHorizontal={8} color={colors.black} fontWeight="bold">
+            EU
+          </Text>
+          <Text color={colors.darkGray} marginHorizontal={8}>
+            US
+          </Text>
+          <Text color={colors.darkGray} marginHorizontal={8}>
+            UK
+          </Text>
+        </XStack>
+      </XStack>
+      <YStack borderBottomColor={colors.blue}
         borderStyle="dotted"
-        marginTop={8}
-      />
-      <View flexDirection="row" justifyContent="space-between">
-        {sizes.map((size) => (
-          <TouchableOpacity
-            key={size}
-            style={[
-              styles.sizeButton,
-              {
-                backgroundColor:
-                  size === selectedSize ? colors.blue : colors.whiteSmoke
-              }
-            ]}
-            onPress={() => { setSelectedSize(size) }}
-          >
-            <Text
-              style={[
-                styles.sizeText,
-                {
-                  color:
-                    size === selectedSize ? colors.whiteSmoke : colors.black
+        marginTop={8} />
+      <XStack justifyContent="space-between">
+        {sizes.map((size) => {
+          const isOutOfStock = outOfStockSizes.includes(size)
+          return (
+            <Button
+              key={size}
+              alignItems="center"
+              backgroundColor={isOutOfStock
+                ? colors.lightGray
+                : size === selectedSize
+                  ? colors.blue
+                  : colors.whiteSmoke}
+              borderColor={colors.whiteSmoke}
+              borderRadius={50}
+              elevation={3}
+              marginVertical={10}
+              shadowColor={colors.black}
+              shadowOffset={{ height: 2, width: 0 }}
+              shadowOpacity={0.1}
+              shadowRadius={9}
+              width={40}
+              onPress={() => {
+                if (!isOutOfStock) {
+                  setSelectedSize(size)
                 }
-              ]}
+              }}
+              disabled={isOutOfStock}
             >
-              {size}
-            </Text>
-          </TouchableOpacity>
-        ))}
-      </View>
-    </View>
+              <Text
+                color={isOutOfStock
+                  ? colors.darkGray
+                  : size === selectedSize
+                    ? colors.whiteSmoke
+                    : colors.black}
+                fontSize={16}
+                fontWeight="500"
+                marginVertical={7}
+              >
+                {size}
+              </Text>
+            </Button>
+          )
+        })}
+      </XStack>
+    </YStack>
   )
 }
-
-const styles = StyleSheet.create({
-  activeSizeOption: {
-    color: colors.black,
-    fontWeight: 'bold'
-  },
-  sizeButton: {
-    alignItems: 'center',
-    borderColor: colors.whiteSmoke,
-    borderRadius: 50,
-    elevation: 3,
-    marginVertical: 10,
-    shadowColor: colors.black,
-    shadowOffset: { height: 2, width: 0 },
-    shadowOpacity: 0.1,
-    shadowRadius: 8,
-    width: 40
-  },
-  sizeOption: {
-    color: colors.darkGray,
-    marginHorizontal: 8
-  },
-  sizeOptions: {
-    flexDirection: 'row'
-  },
-  sizeText: {
-    fontSize: 16,
-    fontWeight: '500',
-    marginVertical: 7
-  }
-})
 
 export default SizeSelector

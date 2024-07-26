8f5fa4a3cbd0827baa67d7e124ada2a2462fa4b6
tudev <tukieu954@gmail.com>
2024-07-26 16:31:55 +0700

    Fix style tamagui, use translation

--- components/atoms/Gallery.tsx
+++ components/atoms/Gallery.tsx
@@ -1,46 +1,44 @@
-import React from 'react'
-import {
-  Image,
-  type ImageSourcePropType,
-  StyleSheet,
-  useColorScheme
-} from 'react-native'
-import { Text, View } from 'tamagui'
+import { useTranslation } from 'react-i18next'
+import { type ImageSourcePropType, TouchableOpacity, useColorScheme } from 'react-native'
+import { Image, Text, View } from 'tamagui'
 
 import getColors from '~/constants/Colors'
 
-const colors = getColors(useColorScheme())
 interface GalleryProps {
   images: ImageSourcePropType[]
   colors: {
     primary: string
     secondary: string
   }
+  scrollToIndex: (index: number) => void
 }
 
-const Gallery: React.FC<GalleryProps> = ({ images, colors }) => {
+const Gallery: React.FC<GalleryProps> = ({ images, scrollToIndex }) => {
+  const { t } = useTranslation()
+  const colors = getColors(useColorScheme())
+
   return (
     <View marginHorizontal={18}>
       <Text fontSize={18} fontWeight={'bold'} marginVertical={7}>
-        Gallery
+        {t('Gallery')}
       </Text>
       <View flexDirection="row" gap={10} justifyContent="flex-start">
         {images.map((img, index) => (
-          <Image key={index} source={img} style={styles.galleryImage} />
+          <TouchableOpacity key={index}
+            onPress={() => { scrollToIndex(index) }}>
+            <Image
+              source={img}
+              backgroundColor={colors.whiteSmoke}
+              borderRadius={10}
+              borderWidth={1}
+              height={60}
+              width={60}
+            />
+          </TouchableOpacity>
         ))}
       </View>
     </View>
   )
 }
-
-const styles = StyleSheet.create({
-  galleryImage: {
-    backgroundColor: colors.whiteSmoke,
-    borderRadius: 10,
-    borderWidth: 1,
-    height: 60,
-    width: 60
-  }
-})
 
 export default Gallery

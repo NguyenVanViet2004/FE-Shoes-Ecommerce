a420997b10adb322287edd1086a4b548e39b1087
Truong <truongtdph40209@fpt.edu.vn>
2024-07-28 12:08:40 +0700

    Add SignInScreen and SignUpScreen for expo-router navigation

---
+++ app/authentication/SignInScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignInTemplate from '~/components/templates/SignInTemplate'
+
+const SignInScreen = (): JSX.Element => {
+  return <SignInTemplate/>
+}
+
+export default SignInScreen
---
+++ app/authentication/SignUpScreen.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignUpTemplate from '~/components/templates/SignUpTemplate'
+
+const SignUpScreen = (): JSX.Element => {
+  return <SignUpTemplate/>
+}
+
+export default SignUpScreen

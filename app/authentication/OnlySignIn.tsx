70c3ad89ebd0b949e870f310822c737c13af14be
Truong <truongtdph40209@fpt.edu.vn>
2024-07-28 13:20:49 +0700

    Change name component OnlySignUp and OnlySignIn

---
+++ app/authentication/OnlySignIn.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignInTemplate from '~/components/templates/SignInTemplate'
+
+const OnlySignIn = (): JSX.Element => {
+  return <SignInTemplate/>
+}
+
+export default OnlySignIn
---
+++ app/authentication/OnlySignUp.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignUpTemplate from '~/components/templates/SignUpTemplate'
+
+const OnlySignUp = (): JSX.Element => {
+  return <SignUpTemplate/>
+}
+
+export default OnlySignUp

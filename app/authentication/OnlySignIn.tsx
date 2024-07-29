49ab894950356571b5d31eeb24e9be9591011259
Truong <truongtdph40209@fpt.edu.vn>
2024-07-29 23:17:51 +0700

    Remove OnlySignUp and OnlySignIn

--- app/authentication/OnlySignIn.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import SignInTemplate from '~/components/templates/SignInTemplate'
-
-const OnlySignIn = (): JSX.Element => {
-  return <SignInTemplate/>
-}
-
-export default OnlySignIn
--- app/authentication/OnlySignUp.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import SignUpTemplate from '~/components/templates/SignUpTemplate'
-
-const OnlySignUp = (): JSX.Element => {
-  return <SignUpTemplate/>
-}
-
-export default OnlySignUp

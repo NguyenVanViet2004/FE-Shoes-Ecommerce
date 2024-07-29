0459d88be1c8d3c0f341c84afbbe3b798c081f0a
Truong <truongtdph40209@fpt.edu.vn>
2024-07-29 21:14:42 +0700

    Remove SignUpScreen and SignUpScreen

--- app/authentication/SignInScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import SignInTemplate from '~/components/templates/SignInTemplate'
-
-const SignInScreen = (): JSX.Element => {
-  return <SignInTemplate/>
-}
-
-export default SignInScreen
--- app/authentication/SignUpScreen.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-import SignUpTemplate from '~/components/templates/SignUpTemplate'
-
-const SignUpScreen = (): JSX.Element => {
-  return <SignUpTemplate/>
-}
-
-export default SignUpScreen

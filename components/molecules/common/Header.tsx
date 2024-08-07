e9798ad903dd921ef41089f99a150d0bf22fbc40
Do Tuan Thanh <tdo29032k4@gmail.com>
2024-08-07 09:05:43 +0700

    update component Header

--- components/molecules/common/Header.tsx
+++ components/molecules/common/Header.tsx
@@ -1,80 +1,70 @@
 import { isNil, isNumber } from 'lodash'
 import React, { type ReactElement } from 'react'
 import { useColorScheme } from 'react-native'
-import { Button, Image, Text, type TextProps, XStack, YStack } from 'tamagui'
+import { Image, Text, type TextProps, XStack, YStack } from 'tamagui'
 
 import getColors from '~/constants/Colors'
-import useTranslation from '~/hooks/useTranslation'
 
 type Props = {
   leftIcon?: number | React.ReactElement
   rightIcon?: number | React.ReactElement
   title?: string
   subtitle?: string
-  centered?: boolean
 } & TextProps
 
-const Header: React.FC<Props> =
-  ({ leftIcon, rightIcon, title, subtitle, centered = true, ...textProp }) => {
-    const colors = getColors(useColorScheme())
-    const { t } = useTranslation()
-    const renderIcon = (icon: number | ReactElement): React.ReactElement => {
-      if (isNumber(icon)) {
-        return <Image source={icon} />
-      }
-      return (
-        <Button
-          unstyled
-          padding={10}
-          borderRadius={50}
-          backgroundColor={colors.white}
-          alignSelf="baseline"
-        >
-          {icon}
-        </Button>
-      )
-    }
+const Header = (props: Props): React.ReactElement => {
+  const colors = getColors(useColorScheme())
 
-    const renderTitleAlone = (title: string): React.ReactElement => {
-      return (
-        <Text
-          fontSize={16}
-          fontWeight={500}
-          {...textProp}>
-          {t('account.' + title)}</Text>
-      )
+  const renderIcon = (icon: number | ReactElement): React.ReactElement => {
+    if (isNumber(icon)) {
+      return <Image source={icon} />
     }
+    return icon
+  }
 
+  const renderTitleAlone = (title: string): React.ReactElement => {
     return (
-      <YStack marginTop={20}>
-        <XStack
-          alignItems="center"
-          justifyContent={centered ? 'space-between' : 'flex-start'}>
-          {!isNil(leftIcon) && renderIcon(leftIcon)}
-
-          {!isNil(title) && (
-            <XStack
-              flex={1}
-              alignItems="center"
-              justifyContent={centered ? 'center' : 'flex-start'}>
-              {renderTitleAlone(title)}
-            </XStack>
-          )}
-
-          {!isNil(title) && !isNil(subtitle) && (
-            <YStack alignItems="center" gap={10}>
-              <Text fontSize={28} fontWeight="bold" color={colors.midnightBlue}>
-                {title}
-              </Text>
-              <Text fontSize={16} fontWeight={500} color={colors.slateGray}>
-                {subtitle}
-              </Text>
-            </YStack>
-          )}
-          {!isNil(rightIcon) && renderIcon(rightIcon)}
-        </XStack>
-      </YStack>
+      <Text
+        fontWeight={500}
+        {...props}>
+        {title}
+      </Text>
     )
   }
+
+  return (
+    <YStack marginTop={20}>
+      <XStack
+        alignItems="center"
+        justifyContent="space-between">
+        {!isNil(props.leftIcon) && renderIcon(props.leftIcon)}
+        {!isNil(props.title) && isNil(props.subtitle) && (
+          <XStack
+            flex={1}
+            alignItems="center"
+            justifyContent="center">
+            {renderTitleAlone(props.title)}
+          </XStack>
+        )}
+        {!isNil(props.title) && !isNil(props.subtitle) && (
+          <YStack
+            marginTop={50}
+            alignItems="center"
+            gap={10}
+            justifyContent="center"
+            flex={1}>
+            <Text fontSize={28} fontWeight="bold" color={colors.midnightBlue}>
+              {props.title}
+            </Text>
+            <Text fontSize={16} fontWeight={500} color={colors.slateGray}>
+              {props.subtitle}
+            </Text>
+          </YStack>
+        )}
+        {!isNil(props.rightIcon) && renderIcon(props.rightIcon)}
+      </XStack>
+    </YStack>
+  )
+}
 
 export default Header

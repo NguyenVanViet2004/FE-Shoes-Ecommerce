7f1f1a5e00883c8fed13f5f4496ddacdf780a5cb
tudev <tukieu954@gmail.com>
2024-08-09 23:09:12 +0700

    Fix repair the interface pulled up

--- components/templates/ProductDetailTemplates.tsx
+++ components/templates/ProductDetailTemplates.tsx
@@ -2,7 +2,7 @@ import Entypo from '@expo/vector-icons/Entypo'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import React, { useRef, useState } from 'react'
 import { Dimensions, FlatList, StatusBar, useColorScheme } from 'react-native'
-import { Button, Image, ScrollView, Theme, XStack, YStack } from 'tamagui'
+import { Button, Image, ScrollView, XStack, YStack } from 'tamagui'
 
 import PriceSection from '~/components/atoms/PriceSection'
 import ProductInfo from '~/components/atoms/ProductInfo'
@@ -30,106 +30,106 @@ const DetailTemplate: React.FC = (): JSX.Element => {
   }
 
   return (
-    <Theme name="light">
-      <YStack>
-        <ScrollView>
-          <StatusBar hidden />
-          <HeaderDetails
-            leftIcon={
-              <Button
-                unstyled
-                padding={10}
-                borderRadius={50}
-                backgroundColor={colors.white}
-                alignSelf="baseline"
-              >
-                <Entypo
-                  name="chevron-left"
-                  size={18}
-                  color={colors.midnightBlue}
-                />
-              </Button>
-            }
-            title="Men's Shoes"
-            rightIcon={
-              <Button
-                unstyled
-                padding={10}
-                borderRadius={50}
-                backgroundColor={colors.white}
-                alignSelf="baseline"
-              >
-                <Ionicons
-                  name="bag-handle-outline"
-                  size={18}
-                  color={colors.midnightBlue}
-                />
-              </Button>
-            }
-          />
-          <YStack backgroundColor={colors.whiteSmoke} height={270}>
-            <FlatList
-              ref={flatListRef}
-              data={images}
-              horizontal
-              pagingEnabled
-              showsHorizontalScrollIndicator={false}
-              onScroll={({ nativeEvent }) => {
-                const index = Math.round(
-                  nativeEvent.contentOffset.x / Dimensions.get('window').width
-                )
-                setCurrentImageIndex(index)
-              }}
-              renderItem={({ item }) => (
-                <Image
-                  source={item}
-                  height={300}
-                  resizeMode="cover"
-                  width={Dimensions.get('window').width}
-                />
-              )}
-              keyExtractor={(item, index) => index.toString()}
-            />
-            <XStack
-              alignItems="center"
-              justifyContent="center"
-              marginVertical={8}
+
+    <YStack flex={1}>
+      <ScrollView>
+        <StatusBar hidden />
+        <HeaderDetails
+          leftIcon={
+            <Button
+              unstyled
+              padding={10}
+              borderRadius={50}
+              backgroundColor={colors.white}
+              alignSelf="baseline"
             >
-              {images.map((_, index) => (
-                <YStack
-                  key={index}
-                  backgroundColor={colors.darkGray}
-                  borderRadius={4}
-                  height={8}
-                  marginHorizontal={4}
-                  opacity={index === currentImageIndex ? 1 : 0.3}
-                  width={8}
-                />
-              ))}
-            </XStack>
-          </YStack>
-          <YStack
-            backgroundColor={colors.white}
-            borderTopRightRadius={25}
-            borderTopLeftRadius={25}
+              <Entypo
+                name="chevron-left"
+                size={18}
+                color={colors.midnightBlue}
+              />
+            </Button>
+          }
+          title="Men's Shoes"
+          rightIcon={
+            <Button
+              unstyled
+              padding={10}
+              borderRadius={50}
+              backgroundColor={colors.white}
+              alignSelf="baseline"
+            >
+              <Ionicons
+                name="bag-handle-outline"
+                size={18}
+                color={colors.midnightBlue}
+              />
+            </Button>
+          }
+        />
+        <YStack backgroundColor={colors.whiteSmoke} height={270}>
+          <FlatList
+            ref={flatListRef}
+            data={images}
+            horizontal
+            pagingEnabled
+            showsHorizontalScrollIndicator={false}
+            onScroll={({ nativeEvent }) => {
+              const index = Math.round(
+                nativeEvent.contentOffset.x / Dimensions.get('window').width
+              )
+              setCurrentImageIndex(index)
+            }}
+            renderItem={({ item }) => (
+              <Image
+                source={item}
+                height={300}
+                resizeMode="cover"
+                width={Dimensions.get('window').width}
+              />
+            )}
+            keyExtractor={(item, index) => index.toString()}
+          />
+          <XStack
+            alignItems="center"
+            justifyContent="center"
+            marginVertical={8}
           >
-            <ProductInfo name="Nike Air Jordan" price="$967.800" />
-            <Gallery
-              images={images}
-              colors={colors}
-              scrollToIndex={scrollToIndex}
-            />
-            <SizeSelector
-              sizes={sizes}
-              selectedSize={selectedSize}
-              setSelectedSize={setSelectedSize}
-              outOfStockSizes={outOfStockSizes}
-            />
-            <PriceSection inStock={true} />
-          </YStack>
-        </ScrollView>
-      </YStack>
-    </Theme>
+            {images.map((_, index) => (
+              <YStack
+                key={index}
+                backgroundColor={colors.darkGray}
+                borderRadius={4}
+                height={8}
+                marginHorizontal={4}
+                opacity={index === currentImageIndex ? 1 : 0.3}
+                width={8}
+              />
+            ))}
+          </XStack>
+        </YStack>
+        <YStack
+          backgroundColor={colors.white}
+          borderTopRightRadius={25}
+          borderTopLeftRadius={25}
+        >
+          <ProductInfo name="Nike Air Jordan" price="$967.800" />
+          <Gallery
+            images={images}
+            colors={colors}
+            scrollToIndex={scrollToIndex}
+          />
+          <SizeSelector
+            sizes={sizes}
+            selectedSize={selectedSize}
+            setSelectedSize={setSelectedSize}
+            outOfStockSizes={outOfStockSizes}
+          />
+          <PriceSection inStock={true} />
+        </YStack>
+      </ScrollView>
+    </YStack>
+
   )
 }
 
